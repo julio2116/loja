db7ac478a4e6dc8e4b072bf8c13cf9dd06fa5c7e
julio2116 <juliobatista2116@gmail.com>
2025-07-23 11:36:28 -0300

    Retornando imagens

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-// https://www.figma.com/design/PNzJHfdLcSDfFes2S67ivg/Cloth-Store-%7C-Fashion-Store-%7C-E-commerce-UI-Kit--Community-?node-id=2-270&t=TnHN0LO7s9fpgrJ2-0
+//https://www.figma.com/design/PNzJHfdLcSDfFes2S67ivg/Cloth-Store-%7C-Fashion-Store-%7C-E-commerce-UI-Kit--Community-?node-id=2-270&t=TnHN0LO7s9fpgrJ2-0
 import Rotas from "./rotas/Rotas";
 function App() {
   return (

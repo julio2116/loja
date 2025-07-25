300587e83c8dc64dc2ac7e96f229a99e0092c312
julio2116 <juliobatista2116@gmail.com>
2025-07-25 12:49:37 -0300

    Inicio da adição dos botoes de favoritos e adicionar ao carrinho

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -1,11 +1,12 @@
 import { useMediaQuery } from "usehooks-ts";
+import cart from "../assets/imagens/cart.svg";
 
 const Card = ({ img, details, inline }) => {
   const desktop = useMediaQuery("(min-width: 1024px)");
   return (
     <>
       <div
-        className={`tracking-normal min-w-[45%] ${
+        className={`tracking-normal min-w-[45%] relative ${
           inline
             ? "lg:min-w-[46%] lg:mx-[2%] 2xl:min-w-[29.33%]"
             : "lg:min-w-[30.33%] lg:mx-[1.5%] 2xl:min-w-[22%]"
@@ -22,6 +23,9 @@ const Card = ({ img, details, inline }) => {
             <div className="text-[12px] lg:text-[18px] font-semibold flex justify-between">
               <span>Full Sleeve Zipper</span>
               <span>$ 199</span>
+            </div>
+            <div className="absolute bottom-[25%] w-[20%] left-[53.3%] translate-x-[-50%]">
+              <img src={cart} alt="" />
             </div>
           </>
         )}
